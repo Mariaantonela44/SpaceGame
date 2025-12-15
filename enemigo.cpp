d93c2ae5776232372a02970ac182f3b918726548
Maria Antonela Romano <antonelaromano016@gmail.com>
2025-12-15 01:27:43 -0300

    agregado de funciones mover, posiciones , borrar, dibujar, estar vivo y daño. FALTA PROBAR

--- enemigo.cpp
+++ enemigo.cpp
@@ -4,3 +4,55 @@
 #include <conio2.h>
 using namespace std;
 
+//posiciones de enemigos 
+int enemigo::getPosx() { 
+	return posx; 
+}
+int enemigo::getPosy() {
+	return posy;
+}
+
+//verificacion si esta vivo 
+bool enemigo::estaVivo() { 
+	return vivo; 
+}
+
+
+void enemigo::mover() {
+	posx = posx + direccion;
+	
+	//limites de borde 
+	if (posx < 2) {
+		posx = 2;
+		direccion = 1;
+		posy=posy+1;
+	}
+	if (posx > 79) {
+		posx = 79;
+		direccion = -1; 
+		posy=posy+1;
+	}
+	//limite extremo inferior
+	if (posy > 20) posy = 20;
+}
+
+void enemigo::borrar() {
+	gotoxy(posx, posy);
+	cout << ' ';
+}
+
+//daño al enemigo 
+void enemigo::danio() {
+	puntosenemigo=puntosenemigo-1;
+	if (puntosenemigo <= 0) {
+		borrar();
+		vivo = false;
+	}
+}
+
+//dibujo de letra de enemigo 
+void enemigo::dibujar() {
+	gotoxy(posx, posy);
+	textcolor(color);
+	cout << forma;
+}
--- enemigo.h
+++ enemigo.h
@@ -3,13 +3,29 @@
 
 class enemigo {
 private:
+	bool vivo;
 	
 protected:
-
+	int velocidad;
+	char forma;
+	int color;
 	
 
 public:
-
+	int direccion;
+	bool ataque;
+	int posx;
+	int posy;
+	int puntosenemigo;
+	enemigo();
+	int getPosx();
+	int getPosy();
+	void borrar();
+	void mover();
+	void dibujar();
+	void danio();
+	bool atacar();
+	bool estaVivo();
 };
 
 
