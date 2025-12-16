2a6f63659d2ebd0f7be2b12a1fc3d537f558cfb5
Maria Antonela Romano <antonelaromano016@gmail.com>
2025-12-15 21:42:58 -0300

    modificacion en juego.cpp error de }

--- juego.cpp
+++ juego.cpp
@@ -119,65 +119,127 @@ void juego::iniciarjuego() {
 	while (juegoActivo) {
 		char tecla = 0;
 		
-		//si pulso una tecla
+		//PRESIONAR TECLA
 		if (_kbhit()){ tecla = getch();
 		}else{ 
 			tecla=0;
 			}
-	
+	    //DIBUJO Y MOVIMIENTO DEL JUGADOR
 		jugador1.dibujar();
 		jugador1.moverjugador(tecla);
 		
-		
+	     //ATAQUE DEL JUGADOR
 		if (tecla == ' ' && !balaJugador.disparoJugador()){
 			balaJugador.activarDisparoJugador(jugador1.getPosx(), jugador1.getPosy());
 		}
-	
+		//BALAS DEL JUGADOR CON TIMER
 		if(clock()-relojBalaJugador>=intervaloBalaJugador){
 		balaJugador.movimientoBalaJugador();
 		relojBalaJugador=clock();}
 		
-		//bala jugador
+
 		if (balaJugador.disparoJugador()) {
 			balaJugador.dibujar();
 		}
 		if (balaJugador.disparoJugador()){ 
 			balaJugador.dibujar();
 		}
-		
-		//bala enemigo
+		//BALAS JUGADOR
 		if (clock() - relojBalaEnemigo >= intervaloBalaEnemigo) {
-		balaEnemigo.movimientoBalaEnemigo();
-		relojBalaEnemigo = clock();
+			balaEnemigo.movimientoBalaEnemigo();
+			relojBalaEnemigo = clock();
 		}
-	
+		
 		if (balaEnemigo.disparoEnemigo()) {
 			balaEnemigo.dibujar();
 		}
 		
-		
-		//Dibujo y movimiento de enemigos 
-		if (clock() - relojEnemigos >= intervaloEnemigos) {
+		//COLISION BALA DEL JUGADOR A ENEMIGO 
+		if(balaJugador.disparoJugador()){
+			int balaX=balaJugador.getPosx();
+			int balaY=balaJugador.getPosx();
+			
 			for (int i = 0; i < MAX_ENEMIGOS; i++) {
-				if (enemigos[i] != nullptr && enemigos[i]->estaVivo()) {
-					enemigos[i]->borrar();
-					enemigos[i]->mover();
-					enemigos[i]->dibujar();
-				
-					if (enemigos[i]->atacar()){
-						balaEnemigo.activarDisparoEnemigo(enemigos[i]->getPosx(),enemigos[i]->getPosy());
-					}
-				
-					if (balaJugador.disparoJugador() && balaJugador.getPosx() == enemigos[i]->getPosx() &&balaJugador.getPosy() == enemigos[i]->getPosy()) {
+				if (enemigos[i] && enemigos[i]->estaVivo()) {
+					int enemigoX=enemigos[i]->getPosx();
+					int enemigoY=enemigos[i]->getPosy();
+					
+					if  (balaY == enemigoY && balaX >= enemigoX && balaX < enemigoX + 3) {
+						
 						aumentarPuntaje(enemigos[i]->puntosenemigo);
 						enemigos[i]->danio();
+						
+						balaJugador.borrar();
 						balaJugador.desactivarDisparoJugador();
+						break;
 					}
 				}
 			}
-			relojEnemigos=clock();
 		}
 		
+		//BALAS ENEMIGAS 
+		
+		//DISPARO DE ENEMIGOS
+		if (clock() - relojEnemigos >= intervaloEnemigos) {
+			
+			bool cambiarDireccion = false;
+			
+			// detectar borde
+			for (int i = 0; i < MAX_ENEMIGOS; i++) {
+				if (enemigos[i] && enemigos[i]->estaVivo()) {
+					int x = enemigos[i]->getPosx();
+					if (x <= 2 || x >= 79) {
+						cambiarDireccion = true;
+						break;
+					}
+				}
+			}	
+			
+		//BAJAR AL COLISIONAR CON BORDES LATERALES 
+			if (cambiarDireccion) {
+				for (int i = 0; i < MAX_ENEMIGOS; i++) {
+					if (enemigos[i] && enemigos[i]->estaVivo()) {
+						enemigos[i]->borrar();
+						enemigos[i]->direccion *= -1;
+						enemigos[i]->posy += 1;
+						enemigos[i]->dibujar();
+					}
+				}
+			}
+			//MOVIMIENTO DE ENEMIGOS
+			for (int i = 0; i < MAX_ENEMIGOS; i++) {
+				if (enemigos[i] && enemigos[i]->estaVivo()) {
+					
+					if (enemigos[i]->getPosy() >= jugador1.getPosy()) {
+						juegoActivo = false;
+						break;
+					}
+					
+					enemigos[i]->borrar();
+					enemigos[i]->mover();
+					enemigos[i]->dibujar();
+				}
+			}
+		
+			//DISPARO DE ENEMIGO DESDE LA ULTIMA FILA 
+			if (!balaEnemigo.disparoEnemigo()) {
+				
+				for (int i = 0; i < MAX_ENEMIGOS; i++) {
+					if (enemigos[i] && enemigos[i]->estaVivo()) {
+						
+						if (!hayEnemigoDebajo(enemigos[i])) {
+							
+							if (enemigos[i]->atacar()) {
+								balaEnemigo.activarDisparoEnemigo(enemigos[i]->getPosx(),enemigos[i]->getPosy());
+								break;
+								}
+							}
+						}
+					}
+				}
+			
+			relojEnemigos = clock();
+		}								
 		// bala enemigo vs jugador
 		if (balaEnemigo.disparoEnemigo() && balaEnemigo.getPosx() == jugador1.getPosx() && balaEnemigo.getPosy() == jugador1.getPosy()) {
 			jugador1.CantidadVidas();
@@ -186,27 +248,21 @@ void juego::iniciarjuego() {
 				juegoActivo = false;
 			}
 		}
-		
-		//bala jugador vs enemigo 
-		for (int i = 0; i < MAX_ENEMIGOS; i++) {
-			if (enemigos[i] && enemigos[i]->estaVivo()) {
-				
-				if (balaJugador.disparoJugador() &&
-					balaJugador.getPosx() == enemigos[i]->getPosx() &&
-					balaJugador.getPosy() == enemigos[i]->getPosy()) {
-					
-					aumentarPuntaje(enemigos[i]->puntosenemigo);
-					enemigos[i]->danio();
-					balaJugador.desactivarDisparoJugador();
-				}
-			}
-		}
-			
-			// --- HUD ARREGLADO --- Problemas que no se veia
-			gotoxy(2, altoBorde + 1);
-			cout << "Vidas: " << jugador1.Vidas()<< "  Puntaje: " << getPuntaje() << "     ";
-	}
 	
+		actualizar1.limpiar();
+		
+		textcolor(12);
+		gotoxy(30, 10);
+		cout << "GAME OVER";
+		
+		textcolor(15);
+		gotoxy(25, 12);
+		cout << "Puntaje final: " << puntaje;
+		
+		gotoxy(20, 15);
+		cout << "Presione una tecla para salir...";
+		getch();
+		
 	// limpiar memoria
 	for (int i = 0; i < MAX_ENEMIGOS; i++)
 		delete enemigos[i];
